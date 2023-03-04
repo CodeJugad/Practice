23ca919985ec8909075d2b47d3582b8c6443435d
yogesh <74785276+Yogi123yadav@users.noreply.github.com>
2023-03-04 16:40:01 +0530

    learning sysout in java

---
+++ src/com/company/SysRunner.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class SysRunner{
+    public static void main(String args[]){
+
+        System1.out1.println1(1);
+        System1.out1.println1('a');
+        System1.out1.println1("hello");
+        System1.out1.println1(1.2);
+        System1.out1.println1(true);
+
+       // System.out.println(1);
+    }
+}
---
+++ src/com/company/System1.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class System1 {
+
+    static System1 out1 = new System1();
+
+    public static void println1(int x){
+      System.out.println(x);
+    }
+    public static void println1(float x){
+        System.out.println(x);
+
+    }
+    public static void println1(double x){
+        System.out.println(x);
+
+    }
+    public static void println1(boolean x){
+        System.out.println(x);
+
+    }
+    public static void println1(char x){
+        System.out.println(x);
+
+    }
+    public static void println1(String x){
+        System.out.println(x);
+
+    }
+}
