bafdf5b066f76bfa46f7d96b49a97892b73d35cb
yogesh <74785276+Yogi123yadav@users.noreply.github.com>
2023-03-05 13:02:13 +0530

    other but useful usage of sysout1

---
+++ src/com/company/PrintStream1.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class PrintStream1 {
+    
+    public static void println1(int x){
+      System.out.println(x);
+    }
+    public static void println1(float x){
+        System.out.println(x);
+
+    }
+    public static void println1(double x) {
+        System.out.println(x);
+    }
+
+}
--- src/com/company/SysRunner.java
+++ src/com/company/SysRunner.java
@@ -5,9 +5,9 @@ public class SysRunner{
 
         System1.out1.println1(1);
         System1.out1.println1('a');
-        System1.out1.println1("hello");
+//        System1.out1.println1("hello");
         System1.out1.println1(1.2);
-        System1.out1.println1(true);
+//        System1.out1.println1(true);
 
        // System.out.println(1);
     }
